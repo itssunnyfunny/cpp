431855f4350bcdc5f1a515ceea0bf4b987330ff1
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-05-09 11:00:44 +0530

    Initial commit and data types in c++

---
+++ 3_primitives/data.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main() {
+    int teaLeaves= 55;
+    float waterTemperature= 100.11;
+    double steepingTime= 5.19; // in minutes
+    char teaType= 'G'; // G for Green, B for Black, H for Herbal
+    bool isSteeping= true;
+
+    cout << isSteeping << endl; // Output: 1 (true)
+    cout << teaLeaves << endl; // Output: 55
+    cout << waterTemperature << endl; // Output: 100.11
+
+    return 0;
+}
