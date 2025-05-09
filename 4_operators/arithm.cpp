af22f9106d67ef4dfe53fa7779c6208f852527d9
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-05-09 14:17:46 +0530

    arithmatic operations

---
+++ 4_operators/arithm.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+
+int main() {
+    int cups;
+    double pricePerCup, totalPrice, discountedPrice;
+
+    cout << "Enter the number of cups: ";
+    cin >> cups;
+    cout << "Enter the price per cup: ";
+    cin >> pricePerCup;
+
+   totalPrice = cups * pricePerCup;
+
+   if (totalPrice > 100) {
+         discountedPrice = totalPrice - (totalPrice * 0.05); //
+         cout << "Discounted price: " << discountedPrice << endl;
+   }
+    else {
+            cout << "Total price: " << totalPrice << endl;
+    }
+
+
+
+}
