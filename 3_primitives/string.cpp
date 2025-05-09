6248c4b3af588d7da9d5c5b2a41d00a75a2950a1
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-05-09 11:51:39 +0530

    strings in c++

---
+++ 3_primitives/string.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+int main() {
+    string says = "Hello, \"World!\""; // Using escape characters for double quotes
+    cout << says << endl; // Output: Hello, World!
+}
