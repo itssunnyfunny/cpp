2e7b56711c1dbd23fa4163be8fd3359f6da806f8
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-05-10 14:40:57 +0530

    for loop in c++

---
+++ 6_Loops/taskThird.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include <string>
+
+
+
+using namespace std;
+
+int main() {
+    int teaCups = 5;
+
+    for (int i = 1; i < teaCups; i++)
+    {
+       cout << "Brewing cup "<< i  << " of tea.\n";
+    }
+}
