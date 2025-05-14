9caf2a607cbdbe01acbb1f4243f327ce5332b62e
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-05-14 11:59:01 +0530

    delegation constructor

---
+++ 9_OPPs/deligation.cpp
@@ -0,0 +1,40 @@
+#include <iostream>       
+#include <string>
+#include <vector>
+
+
+
+using namespace std;
+// delegration constructor
+class Chai{
+    public:
+    string teaName;
+    int teaServings;
+    vector<string> ingredients;
+
+    Chai(string name, int serve, vector<string> ing){
+        teaName = name;
+        teaServings = serve;
+        ingredients = ing;
+        cout << "Parameterized constructor called!" << endl;
+    }
+
+    // delegating constructor
+    Chai(string name) : Chai(name, 1, {"water", "tea leaves"}) {}
+
+        void dispalyCoffeeDetails() {
+                 cout << "Chai Name: " << teaName << endl;
+                cout << "Servings: " << teaServings << endl;
+                cout << "Ingredients: ";
+                for (const auto& ingredient : ingredients) {
+                    cout << ingredient << " ";
+                }
+                cout << endl;
+            }
+};
+
+
+int main() {
+    Chai quickChai("Quick chai");
+    quickChai.dispalyCoffeeDetails();
+}
