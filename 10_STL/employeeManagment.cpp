40a0b09ee3863258e5ba13e6fa2ce9fdf67104db
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-05-18 14:43:37 +0530

    completing the employee mgt stm, with lot of iterators and numberic stl in c++

--- 10_STL/employeeManagment.cpp
+++ 10_STL/employeeManagment.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <deque>    
-#include <list>
-#include <map>
-#include <set>
-#include <unordered_map>
-#include <unordered_set>
-#include <ctime>
+#include <numeric>
+
 #include <algorithm>
 #include <iterator>
 
@@ -29,9 +24,50 @@ void displayEmployee(const Employee& emp) {
 int main() {
 
     vector<Employee> employees = {
-        {1}
+        {1, "shani", 100000},
+        {2, "varun", 200000},
+        {3, "avadesh", 300000},
+        {4, "sanjay", 400000},
+        {5, "ravi", 500000},
+        {6, "mahesh", 600000},
+        {7, "ranjiv", 700000},
+        {8, "rajveer", 800000},
+        {9, "vijay", 900000},
+        {10, "suresh", 1000000},
 
-    }
+    };
+
+    sort(employees.begin(), employees.end(), [](const Employee& a, const Employee& b) {
+        return a.salary < b.salary;
+    });
+
+    cout << "Employees sorted by salary: lowest to highest" << endl;
+
+    for_each(employees.begin(), employees.end(), displayEmployee);
+
+    vector<Employee> highPaidEmployees;
+    copy_if(employees.begin(), employees.end(), back_inserter(highPaidEmployees), [](const Employee& emp) {
+        return emp.salary > 500000;
+    });
+
+    cout << "Employees with salary > 500000: " << endl;
+
+    for_each(highPaidEmployees.begin(), highPaidEmployees.end(), displayEmployee);
+
+
+     double totalSalary = accumulate(employees.begin(), employees.end(), 0.0, [](double sum, const Employee& emp) {
+        return sum + emp.salary;
+    });
+
+    double averageSalary = totalSalary / employees.size();
+
+    cout << "Total salary: " << totalSalary << endl;
+    cout << "Average salary: " << averageSalary << endl;
+
+   auto highestPaid = max_element(employees.begin(), employees.end(), [](const Employee& a, const Employee& b) {
+        return a.salary < b.salary;
+    });
+
     return 0;
 
 }
